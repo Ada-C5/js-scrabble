2e92f456e49a93e362c5e5334368b8a3b644fe90
Jade Vance <jadenicolevance@gmail.com>
2016-06-09 15:30:13 -0700

    removed constant because it does not work, used this.tile_score instead and now it works

--- scrabble.js
+++ scrabble.js
@@ -1,5 +1,5 @@
 var Scrabble = function() {
-  var tile_scores = { 
+  this.tile_score = { 
     'A' : 1, 
     'B' : 3, 
     'C' : 3,
@@ -25,30 +25,35 @@ var Scrabble = function() {
     'W' : 4,
     'X' : 8, 
     'Y' : 4, 
-    'Z' : 10 }
+    'Z' : 10 
+  };
 
-  function score(word) {
-    // sanitize that input! 
-    var word = word.toUpperCase();
-    // split it into letters
-    var letters = word.split("")
-    var score = 0; 
-    // score that mess with a loop
-    // logic to handle 50 point words
-    // return the score 
-
-  }
-  function highestScoreFrom(arrayOfWords) {
-    // Array of words comes in
-    // each word needs to be scored and tracked
-    // 7 letter words + 50 point bonus
-    // return the highest scoring word
-  }
+  // function highestScoreFrom(arrayOfWords) {
+  //   // Array of words comes in
+  //   // each word needs to be scored and tracked
+  //   // 7 letter words + 50 point bonus
+  //   // return the highest scoring word
+  // }
 };
 
 
 Scrabble.prototype.helloWorld = function() {
   return 'hello world!';
 };
+
+Scrabble.prototype.score = function(word) {
+  var word = word.toUpperCase();
+  var score = 0; 
+  
+  // apply that bonus
+  if (word.length >= 7) {
+    score += 50; 
+  }; 
+  // score that mess with a loop
+  for (var i = 0; i < word.length; ++i) {
+    score += this.tile_score[word.charAt(i)] || 0;
+  }
+  return score; 
+}
 
 module.exports = Scrabble;
