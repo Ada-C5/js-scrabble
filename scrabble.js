00a8d1188e1ae30947c5baab63cf22fa4055acd9
Jade Vance <jadenicolevance@gmail.com>
2016-06-09 15:05:45 -0700

    wrote a skeleton for scoring a word as well as an associative array to handle scoring each letter.

--- scrabble.js
+++ scrabble.js
@@ -1,6 +1,52 @@
-var Scrabble = function() {};
+var Scrabble = function() {
+  var tile_scores = { 
+    'A' : 1, 
+    'B' : 3, 
+    'C' : 3,
+    'D' : 2, 
+    'E' : 1, 
+    'F' : 4,
+    'G' : 2, 
+    'H' : 4, 
+    'I' : 1,
+    'J' : 8, 
+    'K' : 5, 
+    'L' : 1,
+    'M' : 3, 
+    'N' : 1, 
+    'O' : 1,
+    'P' : 3, 
+    'Q' : 10, 
+    'R' : 1,
+    'S' : 1, 
+    'T' : 1, 
+    'U' : 1, 
+    'V' : 4, 
+    'W' : 4,
+    'X' : 8, 
+    'Y' : 4, 
+    'Z' : 10 }
 
-// YOUR CODE HERE
+  function score(word) {
+    // sanitize that input! 
+    var word = word.toUpperCase();
+    // split it into letters
+    var letters = word.split("")
+    var score = 0; 
+    // score that mess with a loop
+    // logic to handle 50 point words
+    // return the score 
+
+  }
+  function highestScoreFrom(arrayOfWords) {
+    // Array of words comes in
+    // each word needs to be scored and tracked
+    // 7 letter words + 50 point bonus
+    // return the highest scoring word
+  }
+};
+
+
 Scrabble.prototype.helloWorld = function() {
   return 'hello world!';
 };
