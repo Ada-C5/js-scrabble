383c32be536f0c5b6654da4caa7df79a708f8493
Nadine Curry <nlcurry@gmail.com>
2016-06-09 16:04:28 -0700

    added score function and tilebag object

--- scrabble.js
+++ scrabble.js
@@ -1,8 +1,45 @@
 var Scrabble = function() {};
 
 // YOUR CODE HERE
+var Tilebag{
+  A: 9,
+  B: 2,
+  C: 2,
+  D: 4,
+  E: 12,
+  F: 2,
+  G: 3,
+  H: 2,
+  I: 9,
+  J: 1,
+  K: 1,
+  L: 4,
+  M: 2,
+  N: 6,
+  O: 8,
+  P: 2,
+  Q: 1,
+  R: 6,
+  S: 4,
+  T: 6,
+  U: 4,
+  V: 2,
+  W: 2,
+  X: 1,
+  Y: 2,
+  Z: 1
+};
+
+var score = function(word){
+  // The word is input as a string (case insensitive).
+  // The chart below shows the point value for a given letter.
+  // returns the total score value for the given word.
+}
+
 Scrabble.prototype.helloWorld = function() {
   return 'hello world!';
 };
+
+
 
 module.exports = Scrabble;
