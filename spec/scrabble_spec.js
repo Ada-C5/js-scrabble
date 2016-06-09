614cafb6b2d4ac9aad6a754fc798506992b6028d
justine <justine.winnie@gmail.com>
2016-06-09 15:06:52 -0700

    removing stump test, outlining possible new tests

--- spec/scrabble_spec.js
+++ spec/scrabble_spec.js
@@ -1,9 +1,13 @@
-var Scrabble = require("../scrabble.js");
-
-describe('Hello World', function() {
-  var scrabble = new Scrabble();
-
-  it('says hello world', function() {
-    expect(scrabble.helloWorld()).toEqual('hello world!');
-  });
-});
+// var Scrabble = require("../scrabble.js");
+//
+// describe('Hello World', function() {
+//   var scrabble = new Scrabble();
+//
+//   it('', function() {
+//     expect(scrabble.helloWorld()).toEqual('hello world!');
+//   });
+// });
+//
+// it takes in a string
+//
+// it returns a number
