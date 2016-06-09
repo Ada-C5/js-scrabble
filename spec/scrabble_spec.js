8637ad59b6db7f204fc5ded222ebfa73e3b79790
Jade Vance <jadenicolevance@gmail.com>
2016-06-09 15:47:18 -0700

    wrote more tests against scoring a word

--- spec/scrabble_spec.js
+++ spec/scrabble_spec.js
@@ -7,3 +7,19 @@ describe('Hello World', function() {
     expect(scrabble.helloWorld()).toEqual('hello world!');
   });
 });
+
+describe('score word', function() {
+  var scrabble = new Scrabble();
+
+  it('scores one word', function() {
+    expect(scrabble.score('butts')).toEqual(7);
+  });
+
+  it('properly applies bonus to score', function() {
+    expect(scrabble.score('twilight')).toEqual(65);
+  });
+
+    it('does not score garbage input', function() {
+    expect(scrabble.score('&**#*')).toEqual(0);
+  });
+});
