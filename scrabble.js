cc96308c5bd4a56a57cd642c7d0fb79e102d646c
Jessica <jessica.weeber@gmail.com>
2016-06-10 15:31:24 -0700

    working on highest score from array

--- scrabble.js
+++ scrabble.js
@@ -1,40 +1,65 @@
-'use strict';
-var prompt = require('prompt');
 
-prompt.start();
+var Scrabble = function(x) {
+  this._x = x;
+};
 
-// prompt.get(['word'], scrabble);
+Scrabble.prototype = {
 
-
-var Scrabble = function() {
-  var scoring = {
+  scoring: {
     "A": 1, "E": 1, "I": 1, "O": 1, "U": 1, "L": 1, "N": 1, "R": 1, "S": 1, "T": 1, "D": 2, "G": 2,
     "B": 3, "C": 3, "M": 3, "P": 3, "F": 4, "H": 4, "V": 4, "W": 4, "Y": 4, "K": 5, "J": 8, "X": 8,
     "Q": 10, "Z": 10
-  };
+  },
 
-  var bonus = 50;
+  bonus: 50,
 
-};
+  score: function(_x) {
+    var word = _x;
 
-Scrabble.prototype.score = function(error, word) {
+    if (word.length > 7) {
+        throw "You can only enter a word up to seven letters long."
+    };
 
- if (word.length > 7) {
-      throw "You can only enter a word up to seven letters long."
-  };
+    var wordArray = word.toUpperCase().split('');
 
-  var wordArray = word.toUpperCase().split('');
+    var points = 0;
+    if (word.length == 7) {
+      points += Scrabble.prototype.bonus;
+     }; 
 
-  var points = 0;
-  if (word.length == 7) {
-    points += bonus;
-   }; 
+    for (var letter of wordArray) {
+      points += Scrabble.prototype.scoring[letter];
+    };
 
-  for (var letter of wordArray) {
-    points += scoring[letter];
-  };
+    return points; 
+    },
 
-  return points; 
+  highestScoreFrom: function(_x) {
+    var arrayOfWords = _x;
+
+    var allScores = [];
+      for (var word in arrayOfWords) {
+        allScores.push(Scrabble.prototype.score(word));
+      };
+
+    var pairs = allScores.map { function(arrayOfWords) {}};
+    // example: [[59, "pull"], [43, "cat"], [59, "yes"], [3, "andrea"], [7, "carlos"]]
+    // winners = []
+
+    // pairs.each do |pair|
+    //   if pair[0] == pairs.max[0]
+    //     winners << pair
+    //   end
+    // end
+    //  winners
+    // tie_winner = winners.min_by do |winner|
+    //   winner[1].size
+    // end
+    // return tie_winner[1]
+    return pairs;
+  }
 };
 
 module.exports = Scrabble;
+
+
