06b8e7594072152304c16418a9e7bcc5572e7bad
Jessica <jessica.weeber@gmail.com>
2016-06-09 20:19:36 -0700

    working on starting the prompt

--- scrabble.js
+++ scrabble.js
@@ -1,29 +1,38 @@
+'use strict';
+var prompt = require('prompt');
+
+prompt.start();
+
+// prompt.get(['word'], scrabble);
+
+
 var Scrabble = function() {
-  scoring = {
+  var scoring = {
     "A": 1, "E": 1, "I": 1, "O": 1, "U": 1, "L": 1, "N": 1, "R": 1, "S": 1, "T": 1, "D": 2, "G": 2,
     "B": 3, "C": 3, "M": 3, "P": 3, "F": 4, "H": 4, "V": 4, "W": 4, "Y": 4, "K": 5, "J": 8, "X": 8,
     "Q": 10, "Z": 10
   };
-};
 
-Scrabble.prototype.score = function(word) {
-  
   var bonus = 50;
 
-   if (word.length > 7) {
-        throw "You can only enter a word up to seven letters long."
-    };
+};
 
-    var wordArray = word.toUpperCase().split('');
+Scrabble.prototype.score = function(error, word) {
 
-    var points = 0;
-    if (word.length == 7) {
-      points += bonus;
-     }; 
+ if (word.length > 7) {
+      throw "You can only enter a word up to seven letters long."
+  };
 
-    for (var letter of wordArray) {
-      points += scoring[letter];
-    };
+  var wordArray = word.toUpperCase().split('');
+
+  var points = 0;
+  if (word.length == 7) {
+    points += bonus;
+   }; 
+
+  for (var letter of wordArray) {
+    points += scoring[letter];
+  };
 
   return points; 
 };
