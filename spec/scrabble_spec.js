1980952c5fc947cfc4c853e51b0b55e00f825974
Jade Vance <jadenicolevance@gmail.com>
2016-06-10 15:52:41 -0700

    wrote a test against scoring a word from an array of words

--- spec/scrabble_spec.js
+++ spec/scrabble_spec.js
@@ -1,33 +1,42 @@
 var Scrabble = require("../scrabble.js");
 
-describe('Hello World', function() {
-  var scrabble = new Scrabble();
+  describe('Hello World', function() {
+    var scrabble = new Scrabble();
 
-  it('says hello world', function() {
-    expect(scrabble.helloWorld()).toEqual('hello world!');
-  });
-});
-
-describe('score word', function() {
-  var scrabble = new Scrabble();
-
-  it('scores one word', function() {
-    expect(scrabble.score('butts')).toEqual(7);
+    it('says hello world', function() {
+      expect(scrabble.helloWorld()).toEqual('hello world!');
+    });
   });
 
-  it('properly applies bonus to score of a 7-letter word', function() {
-    expect(scrabble.score('buzzcut')).toEqual(79);
-  });
+  describe('score word', function() {
+    var scrabble = new Scrabble();
 
-  it('properly applies bonus to 7-letter word(refute edition)', function() {
-    expect(scrabble.score('buzzcut')).toNotEqual(29);
-  });
+    it('scores one word', function() {
+      expect(scrabble.score('butts')).toEqual(7);
+    });
 
-  it('does not score garbage input', function() {
-    expect(function(){scrabble.score('&**!#*%');}).toThrow(new Error('this is not valid input!'));
-  });
+    it('properly applies bonus to score of a 7-letter word', function() {
+      expect(scrabble.score('buzzcut')).toEqual(79);
+    });
 
-  it('does not score partially garbage input', function() {
-    expect(function(){scrabble.score('&*avocado!');}).toThrow(new Error('this is not valid input!'));
+    it('properly applies bonus to 7-letter word(refute edition)', function() {
+      expect(scrabble.score('buzzcut')).toNotEqual(29);
+    });
+
+    it('does not score garbage input', function() {
+      expect(function(){scrabble.score('&**!#*%');}).toThrow(new Error('this is not valid input!'));
+    });
+
+    it('does not score partially garbage input', function() {
+      expect(function(){scrabble.score('&*avocado!');}).toThrow(new Error('this is not valid input!'));
+    }); 
+
+  describe('Score an Array of Words', function() {
+    var scrabble = new Scrabble();
+
+    it('scores many words', function() {
+      var test = ['apple']; 
+      expect(scrabble.highestScoreFrom(test)).toEqual([9]);
+    });
   });
 });
