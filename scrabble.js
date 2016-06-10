5c17cd3b125c528dceb3e18ef4e2705c47d6e926
Jessica <jessica.weeber@gmail.com>
2016-06-09 20:02:43 -0700

    set up scoring method

--- scrabble.js
+++ scrabble.js
@@ -1,8 +1,31 @@
-var Scrabble = function() {};
+var Scrabble = function() {
+  scoring = {
+    "A": 1, "E": 1, "I": 1, "O": 1, "U": 1, "L": 1, "N": 1, "R": 1, "S": 1, "T": 1, "D": 2, "G": 2,
+    "B": 3, "C": 3, "M": 3, "P": 3, "F": 4, "H": 4, "V": 4, "W": 4, "Y": 4, "K": 5, "J": 8, "X": 8,
+    "Q": 10, "Z": 10
+  };
+};
 
-// YOUR CODE HERE
-Scrabble.prototype.helloWorld = function() {
-  return 'hello world!';
+Scrabble.prototype.score = function(word) {
+  
+  var bonus = 50;
+
+   if (word.length > 7) {
+        throw "You can only enter a word up to seven letters long."
+    };
+
+    var wordArray = word.toUpperCase().split('');
+
+    var points = 0;
+    if (word.length == 7) {
+      points += bonus;
+     }; 
+
+    for (var letter of wordArray) {
+      points += scoring[letter];
+    };
+
+  return points; 
 };
 
 module.exports = Scrabble;
