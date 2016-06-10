46ebf0e0eba1a6dc5df13d7ce73f469133b10e74
Yordanos <yordanosd@gmail.com>
2016-06-10 15:27:42 -0700

    passed 4 tests for checking if the score function works properly

--- scrabble.js
+++ scrabble.js
@@ -1,8 +1,65 @@
-var Scrabble = function() {};
+var prompt = require('prompt');
 
-// YOUR CODE HERE
-Scrabble.prototype.helloWorld = function() {
-  return 'hello world!';
+var Scrabble = function() {
+  this._points = {
+    A: 1,
+    B: 3,
+    C: 3,
+    D: 2,
+    E: 1,
+    F: 4,
+    G: 2,
+    H: 4,
+    I: 1,
+    J: 8,
+    K: 5,
+    L: 1,
+    M: 3,
+    N: 1,
+    O: 1,
+    P: 3,
+    Q: 10,
+    R: 1,
+    S: 1,
+    T: 1,
+    U: 1,
+    V: 4,
+    W: 4,
+    X: 8,
+    Y: 4,
+    Z: 10
+  };
+};
+
+Scrabble.prototype.score = function(word){
+  var score = 0;
+  var tiles = 0;
+
+  var word_array = word.toUpperCase().split("");
+
+  for(var char of word_array){
+    score += this._points[char] if this._points[char] != NaN;
+
+    tiles += 1;
+  };
+
+  if(tiles == 7){
+    score += 50;
+  };
+  return score;
+};
+
+function input() {
+  prompt.get(['word'], function(error, result) {
+  var word = result['word'];
+  var scrabble = new Scrabble();
+  console.log(scrabble.score(word));
+  });
+
 };
 
 module.exports = Scrabble;
+
+
+prompt.start();
+input();
--- spec/scrabble_spec.js
+++ spec/scrabble_spec.js
@@ -1,9 +1,60 @@
 var Scrabble = require("../scrabble.js");
 
-describe('Hello World', function() {
+describe('Scrabble#score', function() {
   var scrabble = new Scrabble();
+  var points = {
+    A: 1,
+    B: 3,
+    C: 3,
+    D: 2,
+    E: 1,
+    F: 4,
+    G: 2,
+    H: 4,
+    I: 1,
+    J: 8,
+    K: 5,
+    L: 1,
+    M: 3,
+    N: 1,
+    O: 1,
+    P: 3,
+    Q: 10,
+    R: 1,
+    S: 1,
+    T: 1,
+    U: 1,
+    V: 4,
+    W: 4,
+    X: 8,
+    Y: 4,
+    Z: 10
+  };
 
-  it('says hello world', function() {
-    expect(scrabble.helloWorld()).toEqual('hello world!');
+  it('return score of each letter', function() {
+    for(letter in points){
+      expect(scrabble.score(letter)).toEqual(points[letter]);
+    }
   });
+
+  it('return score of 5 for the word cat', function() {
+      expect(scrabble.score('cat')).toEqual(5);
+
+  });
+
+  it('return score of 57 for the word aaaaaaa', function() {
+      expect(scrabble.score('aaaaaaa')).toEqual(57);
+
+  });
+
+  it('return score of 0 if no word is given', function() {
+      expect(scrabble.score('')).toEqual(0);
+
+  });
+
+  it('return score of 0 if no word is given', function() {
+      expect(scrabble.score('c a t')).toEqual(5);
+
+  });
+
 });
