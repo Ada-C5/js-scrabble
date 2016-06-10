c8d82d8bba4ba65dcdadd553fef388ec82c3c796
Lisa Rolczynski <lisa.sano@gmail.com>
2016-06-10 14:33:09 -0700

    wrote tests to check that the score method is functioning and is case insensitive

--- spec/scrabble_spec.js
+++ spec/scrabble_spec.js
@@ -7,3 +7,15 @@ describe('Hello World', function() {
     expect(scrabble.helloWorld()).toEqual('hello world!');
   });
 });
+
+describe('method score(word)', function() {
+  var scrabble = new Scrabble();
+
+  it('returns the score 5 for the word dog', function() {
+    expect(scrabble.score('dog')).toEqual(5);
+  });
+
+  it('is case insensitive', function() {
+    expect(scrabble.score('doG')).toEqual(5);
+  });
+});
