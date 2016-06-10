e02a4d217ae34a1f4e14ac830e4e2d33b7d2cab3
Nicole Iwasaki <Nicole.A.Iwasaki@gmail.com>
2016-06-10 14:25:34 -0700

    scrabble functions highestScoreFrom and checkIfArrayIsUnique

--- spec/scrabble_spec.js
+++ spec/scrabble_spec.js
@@ -7,3 +7,23 @@ describe('Hello World', function() {
     expect(scrabble.helloWorld()).toEqual('hello world!');
   });
 });
+
+describe('Scrabble', function() {
+  var scrabble = new Scrabble();
+
+    it('errors out if non-letters are submitted', function() {
+    expect(scrabble.score("sjgno33")).toEqual("That is not a word!");
+  });
+
+    it('takes in a string and returns a number', function() {
+    expect(scrabble.score("cat")).toEqual(5);
+  });
+});
+
+describe('highest scoring word from array', function() {
+  var scrabble = new Scrabble();
+
+    it('returns highest scoring word from array of words', function() {
+    expect(scrabble.highestScoreFrom(["cat", "dog", "jacuzzi"])).toEqual("jacuzzi");
+  });
+});
