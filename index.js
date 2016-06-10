c3c07921504357d3b73a85abbd8e5729569cb114
melissajimison <melissajimison@gmail.com>
2016-06-09 21:17:17 -0700

    made the highestScoreFrom funtion

--- index.js
+++ index.js
@@ -8,3 +8,44 @@
   we won't be bringing anything into this file. Next project, we'll make good
   use of this file.
 */
+
+var Scrabble = require('./scrabble'),
+    game = new Scrabble(),
+    prompt = require('prompt');
+
+function collect () {
+  // collect some data thte user
+  prompt.get(['word'], function(error, result) {
+    if (/\s/.test(result)){
+       var value = multiple(result)
+    } else {
+      var value = final(result);
+    };
+      console.log(value);
+    ask_again()
+  });
+}
+function ask_again() {
+  prompt.get(["again?"], function(error, result) {
+    var answer = result["again?"];
+    if (answer === "yes") {
+      collect();
+    } else {
+      console.log("bye!");
+      process.exit;
+    };
+  });
+}
+
+function final(result) {
+  var word = result.word;
+  return game.score(word);
+};
+
+function multiple(result) {
+  var words = result.word;
+  return game.highestScoreFrom(words);
+}
+
+prompt.start();
+collect();
--- scrabble.js
+++ scrabble.js
@@ -1,6 +1,8 @@
-var Scrabble = function() {};
+var Scrabble = function() {
+  that = this;
+};
 // YOUR CODE HERE
-var bonus = 50
+var bonus = 50;
 var scores = {
   'A':1, 'E':1, 'I':1, 'O':1, 'U':1, 'L':1, 'N':1, 'R':1, 'S':1, 'T':1, 'D':2, 'G':2,
   'B':3, 'C':3, 'M':3, 'P':3, 'F':4, 'H':4, 'V':4, 'W':4, 'Y':4, 'K':5, 'J':8, 'X':8,
@@ -17,5 +19,18 @@ Scrabble.prototype.score = function(word) {
   };
   return points;
 };
+
+Scrabble.prototype.highestScoreFrom = function(arrayOfWords) {
+  // arrayOfWords = "david jimison"
+  var words_array = arrayOfWords.split(' ');
+  // ["david", "jimison"]
+  var scores_array = words_array.map(function(word) {
+   return that.score(word);
+ });
+  var max = Math.max(...scores_array);
+  var a = scores_array.indexOf(max)
+  return words_array[a]
+};
+
 
 module.exports = Scrabble;
