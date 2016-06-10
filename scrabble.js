918e09263be7824aa57f4dc531d2b7b395008017
Deirdre Storck <deirdre.storck@gmail.com>
2016-06-10 14:54:52 -0700

    wrote methods for score and score highest from, and a few passing tests"

--- scrabble.js
+++ scrabble.js
@@ -1,8 +1,77 @@
 var Scrabble = function() {};
 
-// YOUR CODE HERE
-Scrabble.prototype.helloWorld = function() {
-  return 'hello world!';
+var letter_scores = {
+  A: 1,
+  B: 3,
+  C: 3,
+  D: 1,
+  E: 1,
+  F: 4,
+  G: 2,
+  H: 4,
+  I: 1,
+  J: 8,
+  K: 5,
+  L: 1,
+  M: 3,
+  N: 1,
+  O: 1,
+  P: 3,
+  Q: 10,
+  R: 1,
+  S: 1,
+  T: 1,
+  U: 1,
+  V: 4,
+  W: 4,
+  X: 8,
+  Y: 4,
+  Z: 10
+}
+
+Scrabble.prototype.score = function(word) {
+  upword = word.toUpperCase()
+  score = 0
+  for (var letter of upword) {
+    score += letter_scores[letter]
+  }
+  if (word.length === 7) {
+    score += 50;
+  }
+  return score;
 };
 
+Scrabble.prototype.highestScoreFrom = function(arrayOfWords) {
+  //returns the word in the array with the highest score
+  // var best_word = null;
+  var words_with_scores = {};
+  // by_scores = word_array.group_by { |word| self.score(word.upcase) }
+  for (var word of arrayOfWords) {
+    words_with_scores[word] = this.score(word);
+  }
+  var max = Math.max.apply(null,Object.keys(words_with_scores).map(function(x){ return words_with_scores[x] }));
+  for (var word in words_with_scores){
+    if (words_with_scores[word] === max){
+      best_word = word;
+    }
+  }
+  return best_word;
+};
+
+
 module.exports = Scrabble;
+
+
+
+// var maxKey = _.max(Object.keys(obj), function (o) { return obj[o]; });
+
+
+
+
+// highestScoreFrom(arrayOfWords): returns the word in the array with the highest score.
+//     Note that it’s better to use fewer tiles, so if the top score is tied between multiple words,
+//       pick the one with the fewest letters.
+//     Note that there is a bonus (50 points) for using all seven letters.
+//       If the top score is tied between multiple words and one used all seven letters,
+//       choose the one with seven letters over the one with fewer tiles.
+//       If the there are multiple words that are the same score and same length, pick the first one in supplied list.
--- spec/scrabble_spec.js
+++ spec/scrabble_spec.js
@@ -1,9 +1,27 @@
 var Scrabble = require("../scrabble.js");
 
-describe('Hello World', function() {
+describe('Scrabble', function() {
   var scrabble = new Scrabble();
 
-  it('says hello world', function() {
-    expect(scrabble.helloWorld()).toEqual('hello world!');
+  it('can return score for a basic word', function() {
+    expect(scrabble.score("ada")).toEqual(3);
   });
+
+  it('will give bonus for 7 letter word'), function() {
+    expect(scrabble.score("aaaaaaa")).toEqual(57);
+  };
+
+  it("will return 0 for empty string"), function() {
+    expect(scrabble.score("")).toEqual(0);
+  }
+
+  it('can handle words with mixed upper and lower case'), function() {
+    expect(scrabble.score("aDeI")).toEqual(4);
+  }
+
+  it('can return highest scoring words from array of words'), function() {
+    expect(scrabble.highestScoreFrom(["a", 'd', 'zzz'])).toEqual("zzz");
+  }
+
+
 });
