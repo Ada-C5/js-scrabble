4e892645291967ef097c95422ae4122379ccec77
Adriana <dri19tcc@gmail.com>
2016-06-10 14:36:39 -0700

    First commit forgot.  Added a score function and tests for it.  Adding highestScoreWord

--- scrabble.js
+++ scrabble.js
@@ -1,8 +1,54 @@
 var Scrabble = function() {};
-
 // YOUR CODE HERE
-Scrabble.prototype.helloWorld = function() {
-  return 'hello world!';
+const LETTERS = {
+  A: 1, E: 1, I: 1, O: 1, U: 1, L: 1, N: 1, R: 1, S: 1, T: 1,
+  D: 2, G: 2,
+  B: 3, C: 3, M: 3, P: 3,
+  F: 4, H: 4, V: 4, W: 4, Y: 4,
+  K: 5,
+  J: 8, X: 8,
+  Q: 10, Z: 10
 };
 
+Scrabble.prototype.score = function (word) {
+  var chars     = word.toUpperCase().split(''),
+      sum       = 0;
+  for (i = 0; i < chars.length; i++) {
+    sum += LETTERS[chars[i]];
+    // console.log(chars[i]);
+    // console.log(LETTERS[chars[i]]);
+  }
+  if (word.length === 7) {
+    sum += 50;
+  }
+  return sum;
+}
+
+Scrabble.prototype.highestScoreFrom = function (arrayOfWords) {
+  var high_word = "";
+  for (var arrayOfWord of arrayOfWords) {
+    console.log(arrayOfWord);
+  }
+}
+
+
+
 module.exports = Scrabble;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
--- spec/scrabble_spec.js
+++ spec/scrabble_spec.js
@@ -1,9 +1,33 @@
 var Scrabble = require("../scrabble.js");
 
-describe('Hello World', function() {
+describe ('Score(word)', function() {
   var scrabble = new Scrabble();
 
-  it('says hello world', function() {
-    expect(scrabble.helloWorld()).toEqual('hello world!');
+  it ("returns sum of 5 for the word 'cat'", function() {
+    expect(scrabble.score("cat")).toEqual(5);
+  });
+
+  it ("returns sum of 5 for the word 'aeiou'", function() {
+    expect(scrabble.score("aeiou")).toEqual(5);
+  });
+
+  it ("returns sum of 61 for the word 'tacocat'", function() {
+    expect(scrabble.score("tacocat")).toEqual(61);
+  });
+
+  it ("returns sum of 57 for the word 'aeiouae'", function() {
+    expect(scrabble.score("aeiouae")).toEqual(57);
+  });
+
+  it ("returns sum of 70 for the word 'cacique'", function() {
+    expect(scrabble.score("cacique")).toEqual(70);
+  });
+
+  it ("returns sum of 0 for the word 'empty string'", function() {
+    expect(scrabble.score("")).toEqual(0);
+  });
+
+  it ("returns sum of 61 for the mix cased word 'TacOcAt'", function() {
+    expect(scrabble.score("TacOcAt")).toEqual(61);
   });
 });
