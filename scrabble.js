8018528a792048d37f3040ac48fe3f3cab32d1de
Leah <leahnpetersen@gmail.com>
2016-06-10 14:38:11 -0700

    refactoring array method to check for ties

--- scrabble.js
+++ scrabble.js
@@ -41,25 +41,40 @@ Scrabble.prototype.score = function(word) {
 	for (var i = 0; i < word.length; i++) {
 		// search word[i], get point at to score
 		score += this.point[word[i]]
+		if (word.length == 7) {
+			score += 50;
+		}
 	}
   return score;
 };
 
 Scrabble.prototype.highestScoreFrom = function(arrayOfWords) {
-	var score = 0;
+	// var score = 0;
 	var winner = '';
+	// keep score/word to deduct winner
+	var winner_object = {};
 
 	// sort array in desc order, so smallest wins
-	arrayOfWords.sort(function(a, b){
-  	return b.length - a.length;
-	});
+	// arrayOfWords.sort(function(a, b){
+ //  	return b.length - a.length;
+	// });
 
 	for (var word of arrayOfWords) {
-		word = word.toString().toLowerCase();
-		var word_score = 0;
-		for (var i = 0; i < word.length; i++) {
-			word_score += this.point[word[i]];
-		}
+		// var temp_score = ;
+		winner_object[this.score(word)] = word;
+		console.log(winner_object);
+
+
+
+		// word = word.toString().toLowerCase();
+		// var word_score = 0;
+		// for (var i = 0; i < word.length; i++) {
+		// 	word_score += this.point[word[i]];
+		// }
+
+
+
+
 		// check if player used 7 tiles
 		if (word.length == 7) {
 			word_score += 50;
