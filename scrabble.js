c414732a8088abf73340a3f14fe0f0604f1e3457
Nadine Curry <nlcurry@gmail.com>
2016-06-10 15:27:35 -0700

    added highestscorefrom function

--- scrabble.js
+++ scrabble.js
@@ -1,31 +1,31 @@
 var Scrabble = function() {
-  this._tilebag = {
-    A: 9,
-    B: 2,
-    C: 2,
-    D: 4,
-    E: 12,
-    F: 2,
-    G: 3,
-    H: 2,
-    I: 9,
-    J: 1,
-    K: 1,
-    L: 4,
-    M: 2,
-    N: 6,
-    O: 8,
-    P: 2,
-    Q: 1,
-    R: 6,
-    S: 4,
-    T: 6,
-    U: 4,
-    V: 2,
-    W: 2,
-    X: 1,
-    Y: 2,
-    Z: 1
+  this._score = {
+    A: 1,
+    B: 3,
+    C: 3,
+    D: 2,
+    E: 1,
+    F: 4,
+    G: 2,
+    H: 4,
+    I: 1,
+    J: 8,
+    K: 5,
+    L: 1,
+    M: 3,
+    N: 1,
+    O: 1,
+    P: 3,
+    Q: 10,
+    R: 1,
+    S: 1,
+    T: 1,
+    U: 1,
+    V: 4,
+    W: 4,
+    X: 8,
+    Y: 4,
+    Z: 10
   };
 };
 
@@ -37,7 +37,7 @@ Scrabble.prototype.score = function(word){
 
   if(word.length <= 7){
     for(var letter of word_array){
-      word_score += this._tilebag[letter];
+      word_score += this._score[letter];
     }
 
   // seven letter word gets 50point bonus
@@ -52,9 +52,26 @@ Scrabble.prototype.score = function(word){
 }
 
 
-var highestScoreFrom = function(arrayofWords){
+Scrabble.prototype.highestScoreFrom = function(arrayofWords){
+  var win_score = 0,
+      win_word = arrayofWords[0];
 
-}
+
+  for (var word of arrayofWords){
+    var score_inst = this.score(word);
+    // #checks if current winning score is less than or equal
+    // #to the current word's score, then uses the tiebreaker
+    // #conditions to determine winning word and score
+    if (score_inst > win_score){
+      win_score = score_inst;
+      win_word = word;
+    } else if (score_inst === win_score && word.length < win_word.length){
+      win_word = word;
+      win_score = score_inst;
+    }
+  };
+  return win_word;
+};
 
 Scrabble.prototype.helloWorld = function() {
   return 'hello world!';
