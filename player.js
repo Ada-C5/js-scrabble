34e619b35c1c9b6a96dee4941e7737c7e5d5d484
Adriana <dri19tcc@gmail.com>
2016-06-11 08:16:59 -0700

    Added a plays function for player

--- player.js
+++ player.js
@@ -5,3 +5,13 @@ var Player = function(name) {
   this.name = name;
   this.plays = [];
 };
+
+Player.prototype.plays = function (word) {
+  this.plays.push(word);
+  return this.plays
+}
+
+
+
+
+module.exports = Player;
