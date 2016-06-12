5f5fb03df787bfb9059741c8e9e9bceb2a754c33
Anna Wilson <annie.marie.mason@gmail.com>
2016-06-12 13:57:52 -0700

    player total_score function returns total score, and links with play function to return false if total score is greater than 100

--- player.js
+++ player.js
@@ -3,11 +3,9 @@ var Scrabble = require("./scrabble.js");
 var Player = function(name) {
   this.name = name;
   this.plays = [];
-  this.totalscore = 0;
 };
 
 var Scrabble = new Scrabble;
-//  Scrabble.score("word")
 
 Player.prototype.test = function(word) {
   this._word = word;
@@ -15,32 +13,33 @@ Player.prototype.test = function(word) {
 }
 
 Player.prototype.play = function(word) {
-  if (this.totalscore > 100) {
+  if (this.total_score() > 100) {
     return false;
   } else {
     this.plays.push(word);
   }
-  //returns false if player has already won
 }
 
 Player.prototype.total_score = function() {
-  this.plays.push(word);
-  // - `total_score()`: Function which sums up and returns the score of the players words
+  var total_score = 0;
+
+  for (var word of this.plays) {
+    total_score += Scrabble.score(word);
+  }
+
+  return total_score;
 }
 
 Player.prototype.hasWon = function() {
-  this.plays.push(word);
   // Function with logic to determine if the player has over 100 points,
   // returns `true`, otherwise returns 'false'
 }
 
 Player.prototype.highestScoringWord = function() {
-  this.plays.push(word);
   // Function which returns the highest scoring word the user has played.
 }
 
 Player.prototype.highestWordScore = function() {
-  this.plays.push(word);
   // Function which returns the `highestScoringWord` score.
 }
 
