d8464b3adcd66d479c6533454ae11063acc93adf
Adriana <dri19tcc@gmail.com>
2016-06-12 11:46:29 -0700

    added a hasWon function, return true if points over 99

--- player.js
+++ player.js
@@ -15,7 +15,8 @@ Player.prototype.play = function (word) {
     for (var i = 0; i < word.length; i++) {
       this.played_words.push(word[i]);
     }
-  } else {
+  }
+  else {
     this.played_words.push(word);
   }
   return this.plays();
@@ -26,18 +27,27 @@ Player.prototype.total_score = function () {
   var sum = 0
   for (var i = 0; i < this.played_words.length; i++) {
     sum += scrabble.score(this.played_words[i]);
-    console.log(this.played_words[i]);
   }
   this.score = sum;
   return this.score;
 }
 
-// var adriana = new Player("Adriana");
-// adriana.play("cat");
-// adriana.play("bat");
-// adriana.play("hat");
-// // console.log(adriana.plays());
-//
-// console.log(adriana.total_score());
+Player.prototype.hasWon = function () {
+  if (this.score >= 100) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+var adriana = new Player("Adriana");
+adriana.play("cacique");
+adriana.play("mezquit");
+adriana.play("hat");
+// console.log(adriana.plays());
+
+console.log(adriana.total_score());
+console.log(adriana.hasWon());
 
 module.exports = Player;
