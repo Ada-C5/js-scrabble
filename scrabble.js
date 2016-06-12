916ac382c902ad4fa114e6b00a9ed749a0777f3b
Lisa Rolczynski <lisa.sano@gmail.com>
2016-06-11 19:08:02 -0700

    forgot a semicolon

--- scrabble.js
+++ scrabble.js
@@ -4,7 +4,7 @@ var Scrabble = function() {
     H: 4, I: 1, J: 8, K: 5, L: 1, M: 3, N: 1,
     O: 1, P: 3, Q: 10, R: 1, S: 1, T: 1, U: 1,
     V: 4, W: 4, X: 4, Y: 4, Z: 10
-  }
+  };
 };
 
 Scrabble.prototype = {
