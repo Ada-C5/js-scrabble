f53a04c7cbf3167ae353a3124619b0d533fb10b9
Rowan Cota <cota.rg@gmail.com>
2016-06-12 14:50:16 -0700

    finished wave 2. need to write tests.

--- player.js
+++ player.js
@@ -39,17 +39,14 @@ Player.prototype.hasWon = function () {
   }
 }
 
+Player.prototype.highestScoringWord = function() {
+  var scrabble = new Scrabble();
+  return scrabble.highestScoreFrom(this.plays);
+}
 
-
-// name: property which returns the value of the player's name
-// plays: property which returns an Array of the words played by the player
-// play(word): Function which will adds the input word to the plays Array
-// Returns false if player has already won
-// total_score(): Function which sums up and returns the score of the players words
-// hasWon(): Function with logic to determine if the player has over 100 points, 
-// returns true, otherwise returns 'false'
-// highestScoringWord(): Function which returns the highest scoring word the user has played.
-// highestWordScore(): Function which returns the highestScoringWord score.
-
+Player.prototype.highestWordScore = function() {
+  var scrabble = new Scrabble();
+  return scrabble.score(scrabble.highestScoreFrom(this.plays))
+}
 
 module.exports = Player;
