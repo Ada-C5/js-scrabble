a8672ed6fb683cbcd9ff5c72e0c3bb2e052dec53
Lisa Rolczynski <lisa.sano@gmail.com>
2016-06-11 18:37:11 -0700

    added play(word), total_score(), and hasWon() methods

--- player.js
+++ player.js
@@ -1,5 +1,43 @@
+var Scrabble = require("./scrabble.js");
+var scrabble = new Scrabble();
+
 var Player = function(name) {
   this.name = name;
+  this.plays = [];
+  this.scores = [];
 };
+
+Player.prototype = {
+  play: function(word) {
+
+    if (this.hasWon() === true) {
+      return false;
+    } 
+
+    this.plays.push(word);
+    this.scores.push(scrabble.score(word));
+  },
+  total_score: function() {
+    var sum = this.scores.reduce(
+      function(sum, current){
+        return sum + current;
+      }, 0);
+
+    return sum;
+  },
+  hasWon: function() {
+    var has_won = false;
+    if (this.total_score() >= 100) {
+      has_won = true;
+    }
+    return has_won;
+  },
+  highestScoringWord: function() {
+
+  },
+  highestWordScore: function() {
+
+  }
+}
 
 module.exports = Player;
