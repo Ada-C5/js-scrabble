50f9ffa94d9a902a88d8f63b287731eb845a2bc0
Lisa Rolczynski <lisa.sano@gmail.com>
2016-06-11 20:03:48 -0700

    added tests for the bag initialization and drawTiles() function

--- spec/tile_bag.spec.js
+++ spec/tile_bag.spec.js
@@ -6,4 +6,19 @@ describe('TileBag', function() {
   it('should start with 98 tiles', function() {
     expect(bag.tile_bag.length).toEqual(98);
   });
+
+  describe('drawTiles()', function() {
+    var bag2 = new TileBag();
+
+    it('should remove the number of tiles drawn', function() {
+      bag2.drawTiles(5);
+      expect(bag2.tile_bag.length).toEqual(93);
+    });
+
+    it('should return the correct number of tiles', function() {
+      var bag3 = new TileBag();
+      var pulled = bag3.drawTiles(4);
+      expect(pulled.length).toEqual(4);
+    })
+  });
 });
