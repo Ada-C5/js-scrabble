02aa7a3f68ade76d9589c44b62057eb33ed9b447
Jade Vance <jadenicolevance@gmail.com>
2016-06-12 10:53:58 -0700

    Added in logic to check if the player has won as well as tests to assert and refute winning

--- player.js
+++ player.js
@@ -3,31 +3,38 @@ var scrabble = new Scrabble();
 
 var Player = function(name) {
   this.name = name;
-  this.playedWords = []
+  this.playedWords = new Array; 
 };
 
 Player.prototype.play = function(word) {
   this.playedWords.push(word);
-  return this.playedWords
+  return this.playedWords; 
 };
 
 Player.prototype.totalScore = function(arrayOfWords) {
-
   var totalScore = new Array; 
 
   for (var word of arrayOfWords) {
-    //kittens = 61, puppies = 63, ducklings = 67
     var score = scrabble.score(word); 
     totalScore.push(score);
   }
-
   totalScore = totalScore.reduce(add, 0);
   function add(a, b) {
     return a + b;
   }
-
   return totalScore; 
 }; 
+
+Player.prototype.hasWon = function(arrayOfWords) {
+  if (this.totalScore(arrayOfWords) >= 100) {
+    console.log('YOU WIN');
+    return true; 
+  } else {
+    console.log('You have not won yet.');
+    return false; 
+  }
+};
+
 
 
 module.exports = Player;
@@ -35,7 +42,6 @@ module.exports = Player;
 
 // To do 
 
-// total_score(): Function which sums up and returns the score of the players words
 // hasWon(): Function with logic to determine if the player has over 100 points, returns true, otherwise returns 'false'
 // highestScoringWord(): Function which returns the highest scoring word the user has played.
 // highestWordScore(): Function which returns the highestScoringWord score.
--- spec/player_spec.js
+++ spec/player_spec.js
@@ -2,10 +2,10 @@ var Scrabble = require("../scrabble.js");
 var Player = require("../player.js");
 
 describe('Scrabble Player', function() {
-  var jade = new Player('jade');
+  var jade = new Player('Jade');
 
   it('Knows the name of the player', function() {
-    expect(jade.name).toEqual('jade');
+    expect(jade.name).toEqual('Jade');
   });
 
   it('Allows a player to play a word', function() {
@@ -18,7 +18,19 @@ describe('Scrabble Player', function() {
     expect(jade.play(playAnotherWord)).toEqual(['word', 'kittens']);
   });
 
-    it('Correctly scores player total score', function() {
+  it('Returns true if the player has scored <= 100 points', function() {
+    var andy = new Player('Andy'); 
+    var playSomeWords = ['kittens', 'puppies', 'ducklings']; 
+    expect(andy.hasWon(playSomeWords)).toEqual(true); 
+  }); 
+
+  it('Returns false if the player has scored >= 100 points', function() {
+    var harley = new Player('Harley'); 
+    var playSomeWords = ['bork', 'woof', 'arooo']; 
+    expect(harley.hasWon(playSomeWords)).toEqual(false); 
+  }); 
+
+  it('Correctly scores player total score', function() {
     var andy = new Player('Andy'); 
     var playSomeWords = ['kittens', 'puppies', 'ducklings']; 
     expect(andy.totalScore(playSomeWords)).toEqual(191);
