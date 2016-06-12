088736e21c89e9e7318dfb10c29bacdea56cea87
Alysia Brown <alysiacbrown@gmail.com>
2016-06-12 15:12:51 -0700

    Completed highestScore function.

--- scrabble.js
+++ scrabble.js
@@ -38,23 +38,33 @@ Scrabble.prototype.helloWorld = function() {
 Scrabble.prototype.score = function(word) {
   var word_array = word.toUpperCase().split("")
   score_value = 0
-
   // Applies 50pt bonus for words greater than seven characters long
   if (word_array.length >= 7) {
     score_value += 50;
   }
-
   var self = this;
-
   word_array.forEach(function(letter) {
     score_value += self.score_hash[letter];
   });
-
   return score_value;
-
 };
+
+Scrabble.prototype.highestScoreFrom = function(arrayOfWords) {
+  var score_hash = {}
+  var self = this
+  arrayOfWords.forEach(function(word) {
+    score_hash[word] = self.score(word);
+    score_hash
+  });
+
+  return (Object.keys(score_hash).reduce(function(a, b){ return score_hash[a] > score_hash[b] ? a : b}) + " is worth " + score_hash[Object.keys(score_hash).reduce(function(a, b){ return score_hash[a] > score_hash[b] ? a : b})] + " points so it wins!");
+};
+
+
+
 
 module.exports = Scrabble;
 
 var scrabble = new Scrabble();
-console.log(scrabble.score("hellothere"))
+// console.log(scrabble.score("hellothere"))
+console.log(scrabble.highestScoreFrom(["me", "you", "hello", "hellothere", "hi"]))
