13d3e79e4cf60527d60b002298f1e6aa87d1e4f5
Jade Vance <jadenicolevance@gmail.com>
2016-06-12 10:37:52 -0700

    player can now get a total score of words played. Wrote a test to confirm this code

--- player.js
+++ player.js
@@ -11,4 +11,31 @@ Player.prototype.play = function(word) {
   return this.playedWords
 };
 
+Player.prototype.totalScore = function(arrayOfWords) {
+
+  var totalScore = new Array; 
+
+  for (var word of arrayOfWords) {
+    //kittens = 61, puppies = 63, ducklings = 67
+    var score = scrabble.score(word); 
+    totalScore.push(score);
+  }
+
+  totalScore = totalScore.reduce(add, 0);
+  function add(a, b) {
+    return a + b;
+  }
+
+  return totalScore; 
+}; 
+
+
 module.exports = Player;
+
+
+// To do 
+
+// total_score(): Function which sums up and returns the score of the players words
+// hasWon(): Function with logic to determine if the player has over 100 points, returns true, otherwise returns 'false'
+// highestScoringWord(): Function which returns the highest scoring word the user has played.
+// highestWordScore(): Function which returns the highestScoringWord score.
--- spec/player_spec.js
+++ spec/player_spec.js
@@ -2,13 +2,25 @@ var Scrabble = require("../scrabble.js");
 var Player = require("../player.js");
 
 describe('Scrabble Player', function() {
-  var player = new Player('jade');
+  var jade = new Player('jade');
 
   it('Knows the name of the player', function() {
-    expect(player.name).toEqual('jade');
+    expect(jade.name).toEqual('jade');
   });
 
   it('Allows a player to play a word', function() {
-    expect(player.play('word')).toEqual(['word']);
+    var playAWord = 'word'; 
+    expect(jade.play(playAWord)).toEqual(['word']);
   });
+
+  it('Correctly adds words to the played words array', function() {
+    var playAnotherWord = 'kittens'; 
+    expect(jade.play(playAnotherWord)).toEqual(['word', 'kittens']);
+  });
+
+    it('Correctly scores player total score', function() {
+    var andy = new Player('Andy'); 
+    var playSomeWords = ['kittens', 'puppies', 'ducklings']; 
+    expect(andy.totalScore(playSomeWords)).toEqual(191);
+  }); 
 }); 
