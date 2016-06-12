5d835399520e423a491aebf0374506e00f51bac8
Yordanos <yordanosd@gmail.com>
2016-06-12 14:59:54 -0700

    added plays, play(word), totalScore and hasWon method and passed tests

--- scrabble.js
+++ scrabble.js
@@ -1,65 +1,229 @@
-var prompt = require('prompt');
 
-var Scrabble = function() {
-  this._points = {
-    A: 1,
-    B: 3,
-    C: 3,
-    D: 2,
-    E: 1,
-    F: 4,
-    G: 2,
-    H: 4,
-    I: 1,
-    J: 8,
-    K: 5,
-    L: 1,
-    M: 3,
-    N: 1,
-    O: 1,
-    P: 3,
-    Q: 10,
-    R: 1,
-    S: 1,
-    T: 1,
-    U: 1,
-    V: 4,
-    W: 4,
-    X: 8,
-    Y: 4,
-    Z: 10
+  var prompt = require('prompt');
+
+// SCRABBLE
+  Scrabble = function() {
+    this._points = {
+      A: 1,
+      B: 3,
+      C: 3,
+      D: 2,
+      E: 1,
+      F: 4,
+      G: 2,
+      H: 4,
+      I: 1,
+      J: 8,
+      K: 5,
+      L: 1,
+      M: 3,
+      N: 1,
+      O: 1,
+      P: 3,
+      Q: 10,
+      R: 1,
+      S: 1,
+      T: 1,
+      U: 1,
+      V: 4,
+      W: 4,
+      X: 8,
+      Y: 4,
+      Z: 10
+    };
   };
-};
+  Scrabble.prototype.score = function(word){
+    var score = 0;
+    var tiles = 0;
 
-Scrabble.prototype.score = function(word){
-  var score = 0;
-  var tiles = 0;
+    var wordArray = word.toUpperCase().split("");
 
-  var word_array = word.toUpperCase().split("");
+    for(var char of wordArray){
+      score += this._points[char];
 
-  for(var char of word_array){
-    score += this._points[char] if this._points[char] != NaN;
+      tiles += 1;
+    };
 
-    tiles += 1;
+    if(tiles == 7){
+      score += 50;
+    };
+    return score;
   };
 
-  if(tiles == 7){
-    score += 50;
+  Scrabble.prototype.highestScoreFrom = function(arrayOfWords){
+    // split array
+    // ["cats"], ["hello"],
+    // var arrayOfWords = words;
+    // var  arrayOfWords = [];
+    // arrayOfWords = words;
+    console.log(typeof arrayOfWords);
+
+    var maxWord = [arrayOfWords[0], this.score(arrayOfWords[0])];
+    var currentScore = this.score(arrayOfWords[0]);
+    var highestScoreHash = {
+                      word: (arrayOfWords[0]),
+                      score: currentScore
+                    };
+
+    for(var word of arrayOfWords){
+      var currentScore = this.score(word)
+
+      if (currentScore === highestScoreHash["score"]){
+        if (highestScoreHash["word"].length < 7 && highestScoreHash["word"].length > word.length){
+          highestScoreHash = {
+                            word: word,
+                            score: currentScore
+          }
+        };
+      }else if (currentScore > highestScoreHash["score"]){
+        highestScoreHash = {
+                          word: word,
+                          score: currentScore
+        }
+      };
+    };
+    return highestScoreHash["word"];
   };
-  return score;
-};
 
-function input() {
-  prompt.get(['word'], function(error, result) {
-  var word = result['word'];
-  var scrabble = new Scrabble();
-  console.log(scrabble.score(word));
-  });
+// PLAYER
+  Player = function(name){
+    this.name = name;
+    this.plays = [];
+    this.totalScore = 0;
+    this._tiles = [];
+    this._maxTiles = 7;
+  };
 
-};
+
+
+  Player.prototype.play = function(word){
+    var scrabble = new Scrabble();
+    var score = scrabble.score(word);
+
+    this.plays.push(word);
+    this.totalScore += score;
+
+    return this.hasWon() === true ? false : score;
+  };
+
+  Player.prototype.hasWon = function (){
+    return this.totalScore >= 100 ? true : false;
+  };
+
+  // Player.prototype.wordsAndMatchingScores = function(){
+  //   var totalScoreHash = {};
+  //   var scrabble = new Scrabble();
+  //
+  //   for(var word in this.plays){
+  //     totalScoreHash[word] = scrabble.score(word);
+  //   };
+  //   return totalScoreHash
+  // };
+
+
+  // Player.prototype.totalScore = function(){
+  //   var totalScore = 0;
+  //   for(key in this.wordsAndMatchingScores){
+  //     totalScore += wordsAndMatchingScores[key];
+  //   }
+  //   return totalScore;
+  // }
+
+  // Player.prototype.totalScore = function(){
+  //   var totalScore = 0;
+  //   var scrabble = new Scrabble();
+  //
+  //   for(word in this.plays){
+  //     totalScore += scrabble.score(word);
+  //   }
+  //   return totalScore;
+  // }
+
+  // Player.prototype.highestScore = function(){
+  //   // var highestScore = [];
+  //   // highestScore = wordsAndMatchingScores.max_by {word, score}
+  //   // return highestScore
+  // };
+  //
+  // Player.prototype.highestScoringWord = function(){
+  //   return this.highestScore[0];
+  // };
+  //
+  // Player.prototype.highestWordScore = function(){
+  //   return this.highestScore[1];
+  // };
+
+// class Scrabble::Player
+//   include Scrabble
+//   MAX_TILES_ON_TRAY = 7
+//   attr_reader :name
+//
+//   def initialize(player_info)
+//     @name = player_info[:name]
+//     @plays = []
+//     @tiles = [] # these are the tiles that the player has on their little tray
+//     @tilebag = Scrabble::TileBag.new
+//     draw_tiles(@tilebag)
+//   end
+
+  // # thought about putting this in attr_accessor, but that would let the player edit it in IRB???
+  // def plays
+  //   @plays
+  // end
+
+  // def play(word)
+  //   @plays << word
+  //   score = Scrabble::Scoring.score(word)
+  //   hasWon? == true ? false : score
+  //   #return false if the player has already hasWon
+  //
+  // end
+
+  // def words_and_matching_scores
+  //   total_score_hash = {}
+  //
+  //   plays.each do |word|
+  //     total_score_hash[word] = Scrabble::Scoring.score(word)
+  //   end
+  //   total_score_hash
+  // end
+
+  // def total_score
+  //   # gets value from the scores_for_each_word method
+  //   words_and_matching_scores.values.reduce(:+)
+  // end
+
+  // def hasWon?
+  //   total_score >= 100 ? true : false
+  // end
+
+  // def highest_scoring_word
+  //   highest_score[0]
+  // end
+
+  // def highest_score
+  //   high_score = words_and_matching_scores.max_by do |word, score|
+  //     score
+  //   end
+  //   high_score
+  // end
+
+  // def highest_word_score
+  //   highest_score[1]
+  // end
+
+//   # thought about putting this in attr_accessor, but that would let the player edit it in IRB???
+//   def tiles
+//     @tiles
+//   end
+//
+//   def draw_tiles(tile_bag)
+//     num = MAX_TILES_ON_TRAY - tiles.length
+//     @tiles = tile_bag.draw_tiles(num)
+//   end
+//
+// end
+
+
 
 module.exports = Scrabble;
-
-
-prompt.start();
-input();
--- spec/scrabble_spec.js
+++ spec/scrabble_spec.js
@@ -52,9 +52,194 @@ describe('Scrabble#score', function() {
 
   });
 
-  it('return score of 0 if no word is given', function() {
-      expect(scrabble.score('c a t')).toEqual(5);
+});
 
+
+describe('Scrabble#highestScoreFrom(array_of_words)', function() {
+  var scrabble = new Scrabble();
+
+  // build of array of words and return the highest value word
+
+  it("should return the word 'hello' for the word_array = ['hello']", function(){
+    var arrrayOfWords = ["hello"];
+    expect(scrabble.highestScoreFrom(arrrayOfWords)).toEqual("hello");
   });
 
+  it("should return the word 'hello' as the highest score from the word_array = ['hello', 'cat']", function(){
+    var arrrayOfWords = ["hello", "cat"];
+    expect(scrabble.highestScoreFrom(arrrayOfWords)).toEqual("hello");
+  });
+
+  // # for tiebreaker, the word with less tiles used is returned
+  it("should return the word 'fee' from the word_array = ['cats', 'fee', 'no']", function(){
+    var arrrayOfWords = ["cats", "fee", "no"];
+    expect(scrabble.highestScoreFrom(arrrayOfWords)).toEqual("fee");
+  });
+
+  //  if top score is tied between multiple words but one used all seven letters, that one wins
+  it ("should return the word 'aaaaaaa' from the word_array = ['feet', 'aaaaaad', 'no', 'qqqqqj']", function(){
+    // chose the following words because aaaaaad's score is 58 including the 7-letter bonus,
+    // and qqqqqj's score is also 58 just because of the rare letters
+    var arrayOfWords = ["feet", "aaaaaad", "no", "qqqqqj"];
+    expect(scrabble.highestScoreFrom(arrayOfWords)).toEqual("aaaaaad");
+  });
+
+  it ("should return the word 'aaaaaaa' from the word_array = ['feet', 'qqqqqj, 'no', 'aaaaaad']! BACKWARDS MODE!", function(){
+    // # chose the following words because aaaaaad's score is 58 including the 7-letter bonus,
+    // # and qqqqqj's score is also 58 just because of the rare letters
+    var arrayOfWords = ["feet", "qqqqqj", "no", "aaaaaad"];
+    expect(scrabble.highestScoreFrom(arrayOfWords)).toEqual("qqqqqj");
+  });
+
+  // # testing multiple words with same score and same length
+  it ("should return 'aaaaaaa' from the word_array = ['aaaaaaa', 'eeeeeee', 'frog']", function(){
+    var arrayOfWords = ["aaaaaaa", "eeeeeee", "frog"];
+    expect(scrabble.highestScoreFrom(arrayOfWords)).toEqual("aaaaaaa");
+  });
+
+  it ("should return 'aaaaaad' from the word_array = [''nose'', ''aaaaaad'', ''eeeeeed'', ''frog'', ''rrrrrrr'', ''qqqqqj'']", function(){
+    var arrayOfWords = ["nose", "aaaaaad", "eeeeeed", "frog", "rrrrrrr", "qqqqqj"];
+    expect(scrabble.highestScoreFrom(arrayOfWords)).toEqual("aaaaaad");
+  });
 });
+
+
+//PLAYER spec
+// describe ("Scrabble#Player", function(){
+//   it ("should be an object we have access to", function(){
+//     Scrabble::Player.wont_be_nil
+//   end
+// }):
+
+describe ("Scrabble::Player#name", function(){
+  //  returns the value of the @name instance variable
+  it ("should return the value 'Joe' when player instance is named Joe", function(){
+    var joe = new Player('Joe');
+    // var scrabble = new Scrabble();
+
+    expect(joe.name).toEqual("Joe");
+  });
+});
+
+describe ("Scrabble::Player#plays(word)", function(){
+  var jane = new Player("Jane");
+  jane.play("cat");
+  it ("should return the score '5' for the word 'cat'", function(){
+    expect(jane.plays).toEqual(['cat']);
+  });
+});
+
+describe ("Scrabble::Player#play(word)", function (){
+  var joe = new Player("Joe");
+  var mary = new Player("Mary");
+
+  it ("should return the score '5' for the word 'cat'", function(){
+    expect(joe.play("cat")).toEqual(5);
+  });
+
+
+  it ("should return false if player has already won", function(){
+    // # starts mary off with a bunch of words already played, then adds "cat"
+    var arrayOfWords = ["nose", "aaaaaad", "eeeeeed", "frog", "rrrrrrr", "qqqqqj"];
+
+    // # create an each method that will play the words in the array one by one
+    for(wordPlayed of arrayOfWords){
+      // console.log(wordPlayed);
+      //
+      // console.log(mary.play(wordPlayed));
+      mary.play(wordPlayed);
+
+    };
+    expect(mary.play("cat")).toEqual(false)
+  });
+});
+//
+// describe "Scrabble::Player#total_score" do
+//   jane = Scrabble::Player.new(name: "Jane")
+//   it %!should return '243' for array_of_words = ["nose", "aaaaaad", "eeeeeed", "frog", "rrrrrrr", "qqqqqj"]! do
+//     array_of_words = ["nose", "aaaaaad", "eeeeeed", "frog", "rrrrrrr", "qqqqqj"]
+//
+//     # create an each method that will play the words in the array one by one
+//     array_of_words.each do |word_played|
+//       jane.play(word_played)
+//     end
+//
+//     jane.total_score.must_equal 243
+//   end
+// end
+//
+// describe "Scrabble::Player#highest_scoring_word" do
+//   april = Scrabble::Player.new(name: "April")
+//
+//   # it should return the highest scoring played WORD
+//   it %!should return "frog" for the words played "frog" and "nose"! do
+//     april.play("frog")
+//     april.play("nose")
+//     april.highest_scoring_word.must_equal "frog"
+//   end
+//
+//   it %!should return 'aaaaaad' for array_of_words = ["nose", "aaaaaad", "eeeeeed", "frog", "rrrrrrr", "qqqqqj"]! do
+//     fox = Scrabble::Player.new(name: "Fox")
+//     array_of_words = ["nose", "aaaaaad", "eeeeeed", "frog", "rrrrrrr", "qqqqqj"]
+//     # create an each method that will play the words in the array one by one
+//     array_of_words.each do |word_played|
+//       fox.play(word_played)
+//     end
+//
+//     fox.highest_scoring_word.must_equal "aaaaaad"
+//   end
+//
+//
+// end
+//
+// describe "Scrabble::Player#highest_word_score" do
+//   jody = Scrabble::Player.new(name: "Jody")
+//
+//   # it should return the highest scoring played WORD
+//   it %!should return "8" for the words played "frog" and "nose"! do
+//     jody.play("frog")
+//     jody.play("nose")
+//     jody.highest_word_score.must_equal 8
+//   end
+//
+//   it %!should return '58' for array_of_words = ["nose", "aaaaaad", "eeeeeed", "frog", "rrrrrrr", "qqqqqj"]! do
+//     fox = Scrabble::Player.new(name: "Fox")
+//     array_of_words = ["nose", "aaaaaad", "eeeeeed", "frog", "rrrrrrr", "qqqqqj"]
+//     # create an each method that will play the words in the array one by one
+//     array_of_words.each do |word_played|
+//       fox.play(word_played)
+//     end
+//
+//     fox.highest_word_score.must_equal 58
+//   end
+//
+// end
+//
+// describe "Scrabble::Player#won?" do
+//
+//   it %!should return 'true' for array_of_words = ["nose", "aaaaaad", "eeeeeed", "frog", "rrrrrrr", "qqqqqj"]! do
+//     fox = Scrabble::Player.new(name: "Fox")
+//     array_of_words = ["nose", "aaaaaad", "eeeeeed", "frog", "rrrrrrr", "qqqqqj"]
+//     # create an each method that will play the words in the array one by one
+//     array_of_words.each do |word_played|
+//       fox.play(word_played)
+//     end
+//
+//     fox.won?.must_equal true
+//   end
+//
+//   it %!should return "false" for the words played "frog" and "nose"! do
+//     jody = Scrabble::Player.new(name: "Jody")
+//     jody.play("frog")
+//     jody.play("nose")
+//     jody.won?.must_equal false
+//   end
+// end
+//
+// describe "Scrabble::Player#tiles" do
+//   bill = Scrabble::Player.new(name: "Bill")
+//   # should return a collection of letters that the player can play (max 7) -- that's their own personal little tray of letters
+//   it "should return 7 letters" do
+//     bill.tiles.length.must_equal 7
+//   end
+// end
