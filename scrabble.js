0973c94cbd4ea3e7089303bb475147073c34638f
Cristal Tay <bluecolorpencils@gmail.com>
2016-06-12 16:02:24 -0700

    started the project. ran the 2 npm install commands for save. set up the scrabble.js file to include the 2 fxn names for score and highest word.

--- scrabble.js
+++ scrabble.js
@@ -1,7 +1,12 @@
 var Scrabble = function() {};
 
 // YOUR CODE HERE
-Scrabble.prototype.helloWorld = function() {
+
+Scrabble.prototype.score = function(word) {
+  return 'hello world!';
+};
+
+Scrabble.prototype.highestScoreFrom = function(arrayOfWords) {
   return 'hello world!';
 };
 
