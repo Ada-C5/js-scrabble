53659806a45ea8d3979df217b93267dcbeb71532
sophia <sophiabaldonado@gmail.com>
2016-06-12 16:27:40 -0700

    Refactor highestScoreFrom, implement tied words logic;

--- scrabble.js
+++ scrabble.js
@@ -35,18 +35,87 @@ Scrabble.prototype.score = function(word) {
 }
 
 Scrabble.prototype.highestScoreFrom = function(arrayOfWords) {
-  var word_scores = [];
-  for (word of arrayOfWords) {
-    word_scores.push(this.score(word));
+  // var word_scores = [];
+  // for (word of arrayOfWords) {
+  //   word_scores.push(this.score(word));
+  // }
+  var thisScrabble = this;
+  function assignScores() {
+    thisScrabble.wordsAndScores = {};
+    for (var word of arrayOfWords) {
+      thisScrabble.wordsAndScores[word] = thisScrabble.score(word);
+    }
   }
-  var max = Math.max.apply(Math, word_scores);
-  max_index = word_scores.indexOf(max);
-  return arrayOfWords[max_index];
+
+  function findMax() {
+    assignScores();
+    // console.log(thisScrabble.assignScores)
+    var scores = [];
+    for (var prop in thisScrabble.wordsAndScores) {
+      // if (wordsAndScores[prop] >= max)
+      // console.log(prop)
+      scores.push(thisScrabble.wordsAndScores[prop]);
+    }
+    function compareNumbers(a,b) {
+      return a - b;
+    }
+    var sortedScores = scores.slice(0).sort(compareNumbers);
+
+    // ties?
+    var max = sortedScores[sortedScores.length - 1];
+    var number_of_ties = numberOfTies(sortedScores, max);
+
+    return resolveTies(scores, max, number_of_ties);
+  }
+
+  function numberOfTies(scores, max) {
+    var ties = [];
+    for (var i = 0; i < scores.length; i++) {
+      if (scores[i] === max) { // tie
+        ties.push(scores[i]);
+      }
+    }
+    // console.log("number of ties " + ties.length, ties);
+    return ties.length;
+  }
+
+  function resolveTies(scores, max, amount) {
+    var tiedWordsIndices = [];
+    var currentIndex = 0;
+    for (var val of scores) {
+      if (val === max) {
+        tiedWordsIndices.push(scores.indexOf(val, currentIndex));
+      }
+      currentIndex = scores.indexOf(val);
+    }
+    // console.log("tied " + tiedWordsIndices);
+
+    var tiedWords = [];
+    for (index of tiedWordsIndices) {
+      tiedWords.push(arrayOfWords[index]);
+    }
+    // console.log("tied words " + tiedWords)
+    // best word to choose is 7 letters long or smallest length
+    var bestWord = tiedWords[0];
+    for (var word of tiedWords) {
+      if (word.length >= 7) {
+        bestWord = word;
+      } else if (word.length <= bestWord.length) {
+        bestWord = word;
+      }
+    }
+    return bestWord;
+  }
+
+  return findMax();
+  // var max_index = arrayOfWords.indexOf(max); // this could return the index of the wrong tied word
+  // you can start searching through and array at a specified index withindexOf
+  // we should find the first instance, then keep look thru starting at the index after the first find, instead of having to change the value of the first one for it to ignored or w/e
+  // return arrayOfWords[max_index]; // this could easily return the wrong tied word
 }
 
-
 // var scrabble = new Scrabble();
-
+//
 // scrabble.score("coffee")
 
 
