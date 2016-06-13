49d698088c861fe4e7051dc6843311b1848f9de7
sophia <sophiabaldonado@gmail.com>
2016-06-12 17:01:42 -0700

    Begin setup for player class;

---
+++ player.js
@@ -0,0 +1,27 @@
+var Player = function(name) {
+  name: name,
+  plays: []
+};
+
+
+Player.prototype.play = function(word) {
+
+}
+
+Player.prototype.totalScore = function() {
+
+}
+
+Player.prototype.hasWon = function() {
+
+}
+
+Player.prototype.highestScoringWord = function() {
+
+}
+
+Player.prototype.highestWordScore = function() {
+
+}
+
+module.exports = Player;
