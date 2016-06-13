4518f5483b72a69e0030913bc32ab6e4707e2701
Nadine Curry <nlcurry@gmail.com>
2016-06-12 18:11:20 -0700

    added tests on haswon function and plays property

--- spec/player_spec.js
+++ spec/player_spec.js
@@ -2,8 +2,11 @@ var Player = require("../player.js");
 
 describe('Player', function(){
   var winner = new Player("Wordsmith"),
-      winner._plays = ["develop","jacuzzi"],
       loser = new Player("SilentBob");
+
+  winner.play("jacuzzi");
+  winner.play("develop");
+  console.log(winner._plays);
 
   it("is present", function(){
     expect(winner !== undefined).toEqual(true);
@@ -13,9 +16,27 @@ describe('Player', function(){
     expect(winner._name).toEqual('Wordsmith');
   });
 
-  // test play function
-  it("returns false if player won", function(){
-    expect(winner._name).toEqual('Wordsmith');
+  // test plays property
+  it("returns array of words played", function(){
+    expect(winner._plays).toEqual(["jacuzzi","develop"]);
+    expect(loser._plays).toEqual([]);
   });
+
+  // test total_score function
+
+
+
+  // test haswon function
+  it("returns true if player won", function(){
+    expect(winner.hasWon()).toEqual(true);
+  });
+
+  it("returns false if player doesn't have 100 points", function(){
+    expect(loser.hasWon()).toEqual(false);
+  });
+
+  // test highestscoringword function
+
+  // test highestwordscore function
 
 });
