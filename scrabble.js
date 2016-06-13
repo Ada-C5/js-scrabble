63ba110ddfc8ed28af7eda7c0d314508f7c19f06
Yordanos <yordanosd@gmail.com>
2016-06-12 18:48:57 -0700

    added TileBag object to scrabble.js with a defualtTiles array

--- scrabble.js
+++ scrabble.js
@@ -51,12 +51,6 @@
   };
 
   Scrabble.prototype.highestScoreFrom = function(arrayOfWords){
-    // split array
-    // ["cats"], ["hello"],
-    // var arrayOfWords = words;
-    // var  arrayOfWords = [];
-    // arrayOfWords = words;
-    console.log(typeof arrayOfWords);
 
     var maxWord = [arrayOfWords[0], this.score(arrayOfWords[0])];
     var currentScore = this.score(arrayOfWords[0]);
@@ -95,8 +89,6 @@
     this._scrabble = new Scrabble();
   };
 
-
-
   Player.prototype.play = function(word){
     var score = this._scrabble.score(word);
 
@@ -133,4 +125,34 @@
   };
 
 
+  TileBag = function(){
+    this._default_tiles = [["A", 1], ["A", 1], ["A", 1], ["A", 1], ["A", 1], ["A", 1], ["A", 1], ["A", 1], ["A", 1],
+                  ["B", 1], ["B", 1],
+                  ["C", 1], ["C", 1],
+                  ["D", 1], ["D", 1], ["D", 1], ["D", 1],
+                  ["E", 1], ["E", 1], ["E", 1], ["E", 1], ["E", 1], ["E", 1], ["E", 1],
+                  ["E", 1], ["E", 1], ["E", 1], ["E", 1], ["E", 1],
+                  ["F", 1], ["F", 1],
+                  ["G", 1], ["G", 1],["G", 1],
+                  ["H", 1], ["H", 1],
+                  ["I", 1], ["I", 1], ["I", 1], ["I", 1], ["I", 1], ["I", 1], ["I", 1], ["I", 1], ["I", 1],
+                  ["J", 1],
+                  ["K", 1],
+                  ["L", 1], ["L", 1], ["L", 1], ["L", 1],
+                  ["M", 1], ["M", 1],
+                  ["N", 1], ["N", 1], ["N", 1], ["N", 1], ["N", 1], ["N", 1],
+                  ["O", 1], ["O", 1], ["O", 1], ["O", 1], ["O", 1], ["O", 1], ["O", 1], ["O", 1],
+                  ["P", 1], ["P", 1],
+                  ["Q", 1],
+                  ["R", 1], ["R", 1], ["R", 1], ["R", 1], ["R", 1], ["R", 1],
+                  ["S", 1], ["S", 1], ["S", 1], ["S", 1],
+                  ["T", 1], ["T", 1], ["T", 1], ["T", 1], ["T", 1], ["T", 1],
+                  ["U", 1], ["U", 1], ["U", 1], ["U", 1],
+                  ["V", 1], ["V", 1],
+                  ["W", 1], ["W", 1],
+                  ["X", 1],
+                  ["Y", 1], ["Y", 1],
+                  ["Z", 1]];
+  };
+  
 module.exports = Scrabble;
