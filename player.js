84a7d133cc7b19eca6e1e03974bd8b4416e288fc
Cristal Tay <bluecolorpencils@gmail.com>
2016-06-12 18:14:41 -0700

    added play, total score and haswon function to player.js. they all reference each other. added tests for each of the fxns. all tests pass. onto highest score fxns.

--- player.js
+++ player.js
@@ -2,21 +2,33 @@ var Scrabble = require("./scrabble.js");
 var scrabble = new Scrabble();
 
 var Player = function(name) {
-  this._name = name;
+  this.name = name;
+  this.plays = [];
 };
 
-// Player.protoype.play = function(word) {
-//
-// };
-//
-// Player.prototype.total_score = function() {
-//
-// };
-//
-// Player.prototype.hasWon = function() {
-//
-// };
-//
+Player.prototype.play = function(word) {
+  // add word to array of played words
+  // return word
+  (this.hasWon())? false : (scrabble.score(word) !== 'Unacceptable word!!')? this.plays.push(word) : scrabble.score(word);
+  return scrabble.score(word);
+};
+
+Player.prototype.total_score = function(arrayOfWords) {
+  var score = 0;
+  for (var word of arrayOfWords) {
+    score += scrabble.score(word);
+  };
+  return score;
+};
+
+Player.prototype.hasWon = function() {
+  if (this.total_score(this.plays) >= 100) {
+    return true;
+  } else {
+    return false;
+  };
+};
+
 // Player.prototype.highestScoringWord = function() {
 //
 // };
--- spec/player_spec.js
+++ spec/player_spec.js
@@ -2,7 +2,7 @@ var Scrabble = require("../scrabble.js");
 var Player = require("../player.js");
 
 
-describe('Player JS', function() {
+describe('Player JS file', function() {
   var player = new Player('Zeek');
 
   it('says hello world', function() {
@@ -10,6 +10,35 @@ describe('Player JS', function() {
   });
 
   it('knows the players name', function() {
-    expect(player._name).toEqual('Zeek');
+    expect(player.name).toEqual('Zeek');
   });
+
+  it('knows the players array of played words', function() {
+    expect(player.plays).toEqual([]);
+  });
+
+  it('can get a score from playing a word', function() {
+    expect(player.play('Meow')).toEqual(9);
+  });
+
+  it('tells player if word is not acceptable, does not add to array', function() {
+    expect(player.play('NOTaWord')).toEqual('Unacceptable word!!');
+  });
+
+  it('can add to array of played words', function() {
+    expect(player.plays).toEqual(['Meow']);
+  });
+
+  it('can find total score from an array of words', function() {
+    expect(player.total_score(['Z', 'ZZ', 'ZZZ', 'ZZZZ', 'AAAAA'])).toEqual(105);
+  });
+
+  it('knows if player has won', function() {
+    player.play('ZZZZZZZ');
+    player.play('ZZZZZZZ');
+    expect(player.total_score(player.plays)).toEqual(129);
+    expect(player.hasWon()).toEqual(true);
+  });
+
+
 });
